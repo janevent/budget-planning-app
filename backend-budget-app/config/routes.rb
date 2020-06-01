e5d002158a2a29d7cb2e4edaa80f15dfbde24eed
janeventer <janeanneventer@gmail.com>
2020-05-31 20:50:45 -0700

    add user resources and sessions login and logout

--- backend-budget-app/config/routes.rb
+++ backend-budget-app/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  get 'login', to: 'sessions#create', as: :login
+  get 'logout', to: 'sessions#destroy', as: :logout
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
