c2517dbde47db5f8ec1e4f30f561db728cc27940
janeventer <janeanneventer@gmail.com>
2020-06-01 14:14:00 -0700

    add a show action to UsersController and create a user in seeds.db

--- backend-budget-app/app/controllers/users_controller.rb
+++ backend-budget-app/app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    def create 
+    end
+
+    def show 
+        find_user
+        render json: @user
+    end
+
+    private 
+
+    def find_user 
+        @user = User.find(params[:id])
+    end
 end
