9e3deb58ee2fc3b7b24e9d265f63112b7eaa4eee
janeventer <janeanneventer@gmail.com>
2020-06-10 22:29:02 -0700

    control the value of the input fields with props passed in from NewTemplateForm

--- frontend-budget-app/src/components/IncomeInput.js
+++ frontend-budget-app/src/components/IncomeInput.js
@@ -3,9 +3,9 @@ import React from 'react';
 const IncomeInput = (props) => {
     return (
         <div className="IncomeInput">
-            <label>Description <input type="text" name="description" value={props.income.description} handleChange={ (event) => props.handleIncomeChange(event, props.id)} />
+            <label>Description <input type="text" name="description" value={props.income.description} onChange={ (event) => props.handleIncomeChange(event, props.id)} />
             </label>
-            <label>Amount <input type="text" name="amount" value={props.incme.amount} handleChange={ (event) => props.handleIncomeChange(event, props.id)}/>
+            <label>Amount $<input type="text" name="amount" value={props.income.amount} onChange={ (event) => props.handleIncomeChange(event, props.id)}/>
             </label>
         </div>
     )
