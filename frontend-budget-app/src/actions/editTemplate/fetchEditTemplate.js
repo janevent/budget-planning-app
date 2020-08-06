b19279b1ec61238447e7d4153b7d4a78bcd257c4
janeventer <janeanneventer@gmail.com>
2020-08-06 14:18:16 -0700

    write a fetchEditTemplate method to send a put fetch request in order to store edits to a template

--- frontend-budget-app/src/actions/editTemplate/fetchEditTemplate.js
+++ frontend-budget-app/src/actions/editTemplate/fetchEditTemplate.js
@@ -5,3 +5,21 @@ const editTemplate = (template, id) => {
         id: id
     }
 }
+
+const fetchEditTemplate = (template, id) => {
+    return (
+        (dispatch) => {
+            fetch(`http//:3000:localhost/templates/${id}`, {
+                method: 'PUT',
+                body: JSON.stringify(template)
+            })
+            .then(resp => resp.json())
+            .then( json => {
+                dispatch(editTemplate(template, id))
+            })
+        }
+    )
+
+}
+
+export default fetchEditTemplate;
