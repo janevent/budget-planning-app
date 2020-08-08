5b81461a5cea994b547c75472ca649e46d24562e
janeventer <janeanneventer@gmail.com>
2020-08-08 12:44:44 -0700

    check to see if there is data in props before trying to display data.title and rest of form

--- frontend-budget-app/src/components/EditForm.js
+++ frontend-budget-app/src/components/EditForm.js
@@ -1,27 +1,24 @@
 import React from 'react';
 
-export default class EditForm extends React.Component {
+const EditForm = ( { data })  => {
 
-    constructor(props){
-        super(props);
-        console.log('edit form constructor:', this.props.data)
-        this.state = {
-            data: this.props.data
-        }
-    }
-
-    componentDidMount(){
-        console.log('edit form from componenetDidMount:', this.props)
-    }
-    render(){
+    
+    
         return(
+            
             <div className='EditForm'>
-                <p>This is an EditForm</p>
-                <form>
-                    <label>Title: <input name='title' value={this.state.data.title} />
-                    </label>
-                </form>
-            </div>
+                { data ?
+                    <div className='edit-form-wrapper' >
+                        <p>This is an EditForm</p>
+                        <form>
+                            <label>Title: <input name='title' value={data.title} />
+                            </label>
+                        </form>
+                    </div>
+                :
+                "" }
+            </div>   
         )
-    }
 }
+
+export default EditForm
