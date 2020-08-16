9ffa694d043d49cba629cc20855c0b822c6015dd
janeventer <janeanneventer@gmail.com>
2020-08-16 13:56:40 -0700

    remove console.logs

--- frontend-budget-app/src/reducers/templates.js
+++ frontend-budget-app/src/reducers/templates.js
@@ -2,8 +2,6 @@ export default (state =  [], action) => {
     switch(action.type){
         case 'GET_TEMPLATES':
             //what if there are no templates?
-            //return action.user.included[1].attributes
-            console.log("t-action:", action)
             return action.templates
         case 'ADD_TEMPLATE':
             return [...state, action.template];
@@ -13,15 +11,14 @@ export default (state =  [], action) => {
             let lastPart = state.slice(index+1);
             return [...firstPart, ...lastPart]
         case 'EDIT_TEMPLATE':
+            //do I need this if I have been updated throughout
             let i = state.findIndex( (template) => template.id === action.id);
             let template = state.template;
-            //?
             let first = state.slice(0, i);
             let last = state.slice( i+1);
             return [...first, template, ...last]
 //different action types for edits then create new ones
         case 'UPDATE_TITLE':
-            console.log('made it to reducer')
             let id = action.id;
             let template1 = state.find( (template) => {
                 return template.id === id
@@ -31,26 +28,31 @@ export default (state =  [], action) => {
             let templateWTitleUpdate = Object.assign( {}, template1, {title: action.title});
             let firstTemplates = state.slice(0, template1Index);
             let lastTemplates = state.slice(template1Index+1);
-            console.log('action.title', action.title, 'template1', template1, 'templateWTitleUpdate', templateWTitleUpdate)
-            let newTemplates = [...firstTemplates, templateWTitleUpdate, ...lastTemplates];
-            console.log('newTemplates:', newTemplates)        
+            let newTemplates = [...firstTemplates, templateWTitleUpdate, ...lastTemplates];       
             return newTemplates;
             
         case 'UPDATE_INCOME':
+            //helper methods?
+            console.log('action:', action)
             let templateToUpdateIncome = state.find( (template) => {
                 return template.id === action.templateId
             });
+            let indexOfTemplate = state.findIndex( (template) => {
+                return template.id === action.templateId
+            })
+            let firstPartTemplates = state.slice(0, indexOfTemplate);
+            let lastPartTemplates = state.slice( indexOfTemplate+1)
             let indexOfIncome = templateToUpdateIncome.incomes.findIndex( (income) => {
                 return income.id === action.incomdId
             });
             let firstPartIncomes = templateToUpdateIncome.incomes.slice(0, indexOfIncome);
             let lastPartIncomes = templateToUpdateIncome.incomes.slice(indexOfIncome + 1);
             let updatedIncomes = [...firstPartIncomes, action.income, ...lastPartIncomes];
-            let templateWUpdatedIncome = [...state, {...templateToUpdateIncome, ...{incomes: updatedIncomes} } ];
-            return templateWUpdatedIncome;
-
-           // incomeId
-            //templateId
+            let templateWUpdatedIncome = {...templateToUpdateIncome, ...{incomes: updatedIncomes} } ;
+            let updatedTemplates = [...firstPartTemplates, templateWUpdatedIncome, ...lastPartTemplates];
+            console.log('updatedTemplates:', updatedTemplates)
+            return updatedTemplates;
+            
         default:
             return state
     }
