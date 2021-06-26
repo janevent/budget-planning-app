c616925c25e6b6761a76a4d3daa4fbcc95ae16fc
janeventer <janeanneventer@gmail.com>
2021-06-25 18:31:33 -0700

    write functions to handle updates in edited budget

--- frontend-budget-app/src/components/EditBudgetContainer.js
+++ frontend-budget-app/src/components/EditBudgetContainer.js
@@ -14,6 +14,111 @@ import updateTotalIncomes from '../actions/editBudget/updateTotalIncomes';
 
 function EditBudgetContainer(props){
 
+    const handleTitleChange = (event) => {
+        event.persist();
+        let iD = props.match.params.id;
+        props.updateTitle(event.target.value, iD)
+    }
+
+    const handleIncomeChange = (event, incomeId) => {
+        const { name, value } = event.target;
+        let budget = getBudget();
+        let income = budget.incomes.find( income => income.id === incomeId);
+        let updatedAttributes = {...income.attributes, ...{[name]: value} };
+        let updatedIncome = {...income, ...{attributes: updatedAttributes}}
+        let budgetId = props.match.params.id;
+        props.updateIncome(updatedIncome, incomeId, budgetId)
+        totalIncomes();
+    }
+
+    const handleExpenseChange = (event, expenseId) => {
+        event.persist();
+        const { name, value } = event.target;
+        let budget = getBudget();
+        let budgetId = props.match.params.id;
+        const matchExpense = (expense) => expense.id === expenseId
+        let expense = budget.expenses.find( matchExpense );
+        let updatedAttributes = {...expense.attributes, ...{[name]:value}}
+        let updatedExpense = {...expense, ...{ attributes: updatedAttributes} }
+        //console.log('updatedExpense:', updatedExpense)
+        props.updateExpense(updatedExpense, expenseId, budgetId);
+        totalExpenses();
+    }
+
+    const totalExpenses = () => {
+        let expenses = this.getBudget().expenses.map( (expense) => parseInt(expense.attributes.amount) )
+        let totalExs = expenses.reduce(addFunc, 0);
+        let budgetId = props.match.params.id;    
+        props.updateTotalExpenditure(totalExs, budgetId);
+    }
+
+    const totalIncomes = () => {
+        let incomes = getBudget().incomes.map( (income) => {
+            return parseInt(income.attributes.amount)
+        });
+        let totalIncome = incomes.reduce(addFunc, 0);
+        let budgetId = props.match.params.id;
+        console.log('updatedTotalIncome', totalIncome)
+        props.updateTotalIncomes(totalIncome, budgetId)
+    }
+
+    const addFunc = (total, number) => {
+        return total + number
+    }
+
+    const totalDifference = () => {
+        let budgetId = props.match.params.id
+        let budget = props.budgets.find(t => t.id === budgetId)
+        let totalDif = budget.total_income - budget.total_expenditure;
+        props.updateTotalDifference(totalDif, budgetId)
+    }
+
+    const saveEdit = () => {        
+        let budget = getBudget();
+        //temp as argument...
+        console.log('reached SaveEdit')
+        let filteredIncomes = budget.incomes.filter( (i) => {
+            return i.attributes.description !== ""
+        });
+        let filteredExpenses = budget.expenses.filter( (e) => {
+            return e.attributes.description !== ""
+        });
+        let copyIncomes = [...filteredIncomes];
+        let copyExpenses = [...filteredExpenses];
+        copyIncomes.forEach( (i) => i.attributes.id = i.id);
+        //if i.id exists set id in attributes, else skip
+        copyExpenses.forEach( (e) => e.attributes.id = e.id);
+        let newIncomes = copyIncomes.map( (income) => income.attributes);
+        let newExpenses = copyExpenses.map( (expense) => expense.attributes);
+        let newBudget = {
+            title: budget.title,
+            total_income: budget.total_income,
+            total_expenditure: budget.total_expenditure,
+            total_difference: budget.total_difference,
+            expenses_attributes: newExpenses,
+            incomes_attributes: newIncomes,
+            id: budget.id
+        }
+        props.fetchEditBudget(newBudget, budget.id);
+        props.history.push(`/budgets/${budget.id}`)
+    }
+
+    const getBudget = () => {
+        let id = props.match.params.id;
+        let budget = props.budgets.find( (bud) => bud.id === id )       
+        return budget
+    }
+
+    const onClickAddExpense = () => {
+        let budgetId = props.match.params.id;
+        props.addNewExpense(budgetId);
+    }
+
+    const onClickAddIncome = () => {
+        let templateId = props.match.params.id;
+        props.addNewIncome(budgetId);
+    }
+
     return (
         <div className='edit-container'>
             <EditForm />
